82cc345d19962a211a5416a4a994415b9b71ac5b
Michael_Hoffman_26 <michawlhoffman7890@gmail.com>
2024-11-11 20:37:09 +0200

    chore: setup fullstack project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# node-ts-react
-Fullstack repo project
+# Fullstack main repo server
+
+## Local Installation
+
+First, you need to have `nvm` installed.
+Build the server, at the main folder
+
+
+```bash
+nvm use
+npm ci
+npm run start:dev
+```
---
+++ backend/routes/charge/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+import { validateRequest } from '../../validator/validator';
+import { chargeCreditCard } from '../../controller/charge';
+import { postChargeValidator, } from './validators';
+
+const router = express.Router();
+
+
+router.post('',
+    postChargeValidator,
+    validateRequest,
+    chargeCreditCard
+);
+
+export default router;
